183ff6dbb4a8c196c9b377b373042da3c441f919
Bognar Junior <bognar_junior@yahoo.com.br>
2025-10-01 10:29:53 -0300

    Update jest
    Update jest setup and snapshot Home

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,32 @@
 import '@testing-library/jest-native/extend-expect';
+import 'react-native-gesture-handler/jestSetup';
 
-jest.mock('react-native-gesture-handler', () => ({}));
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
+
+jest.mock(
+  'react-native/Libraries/Animated/NativeAnimatedHelper',
+  () => ({}),
+  { virtual: true }
+);
+
+jest.mock(
+  '@react-native-async-storage/async-storage',
+  () => require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
+
+jest.mock('react-native-linear-gradient', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+  return ({ children, ...rest }: any) =>
+    React.createElement(View, { ...rest, testID: 'linear-gradient' }, children);
+});
+
+jest.mock('@react-native-community/datetimepicker', () => 'DateTimePicker');
+
+jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon');
 
 jest.mock('@react-navigation/drawer', () => ({
   createDrawerNavigator: jest.fn().mockReturnValue({
@@ -16,21 +42,8 @@ jest.mock('@react-navigation/native', () => {
     useNavigation: () => ({
       navigate: jest.fn(),
       goBack: jest.fn(),
+      dispatch: jest.fn(),
     }),
-    useRoute: () => ({
-      params: {},
-    }),
+    useRoute: () => ({ params: {} }),
   };
 });
-
-jest.mock('react-native-safe-area-context', () => {
-  const actual = jest.requireActual('react-native-safe-area-context');
-  return {
-    ...actual,
-    useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
-  };
-});
-
-jest.mock('react-native-reanimated', () =>
-  require('react-native-reanimated/mock')
-);
